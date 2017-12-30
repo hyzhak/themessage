221e19aff3622cd454a6a2bbeac368ea2a8b05a2
Eugene Krevenets <ievgenii.krevenets@gmail.com>
2017-12-30 01:55:37 +0100

    if md doesn't have start header we won't have title

--- themessage/markdown_test.py
+++ themessage/markdown_test.py
@@ -1,14 +1,27 @@
 from themessage import markdown
 
 
-def test_get_header_of_md_with_header():
-    md = '# Header !\n' \
-         'some additional text\n' \
-         '# blah-blah-blah\n' \
-         'Hello World!'
+def test_header_of_md_with_header_is_header():
+    md = '# A Vision in a Dream.\n' \
+         'In Xanadu did Kubla Khan\n' \
+         'A stately pleasure-dome decree:\n' \
+         'Where Alph, the sacred river, ran\n' \
+         'Through caverns measureless to man\n' \
+         '    Down to a sunless sea.\n' \
+         '# The End'
 
-    assert markdown.get_header(md) == 'Header !'
+    assert markdown.get_header(md) == 'A Vision in a Dream.'
 
 
-def test_get_header_of_emtpy_md_is_none():
+def test_header_of_an_empty_md_is_none():
     assert markdown.get_header('') is None
+
+
+def test_header_of_an_md_without_header_on_start_is_none():
+    md = 'So twice five miles of fertile ground\n' \
+         'With wa\'lls and towers were girdled round:\n' \
+         'And there were gardens bright with sinuous rills,\n' \
+         'Where blossomed many an incense-bearing tree;\n' \
+         'And here were forests ancient as the hills,\n' \
+         'Enfolding sunny spots of greenery.\n'
+    assert markdown.get_header(md) is None
